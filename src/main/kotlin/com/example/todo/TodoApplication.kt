e7d60553e960d86a3aa75f47ec004add03a4c836
Andre Filliettaz <andrentaz@gmail.com>
2020-04-10 12:58:16 -0300

    Setup Hello World server

--- src/main/kotlin/com/example/todo/TodoApplication.kt
+++ src/main/kotlin/com/example/todo/TodoApplication.kt
@@ -2,9 +2,18 @@ package com.example.todo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class TodoApplication
+@RestController
+class TodoApplication {
+
+	@GetMapping("/hello")
+	fun hello(@RequestParam(value = "name", defaultValue = "World")
+			  name: String) = "Hello $name"
+}
 
 fun main(args: Array<String>) {
 	runApplication<TodoApplication>(*args)
