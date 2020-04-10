995a0508a68c5b16ed4af57c011813f7d7fcf8d2
Andre Filliettaz <andrentaz@gmail.com>
2020-04-10 13:23:32 -0300

    Create todo controller and model

---
+++ src/main/kotlin/com/example/todo/controllers/TodoController.kt
@@ -0,0 +1,17 @@
+package com.example.todo.controllers
+
+import com.example.todo.models.TodoModel
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TodoController {
+
+    @GetMapping("/todos/{id}")
+    fun getTodo(@PathVariable(value = "id") id: String): TodoModel {
+        val templateTitle = "This is my todo"
+        val templateDescription = "This is my todo's description"
+        return TodoModel(id = id, title = templateTitle, description = templateDescription)
+    }
+}
