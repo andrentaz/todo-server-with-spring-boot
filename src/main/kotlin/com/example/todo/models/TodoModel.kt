d6d27d2c876230d89c850c5eb9e8d7526411da46
Andre Filliettaz <andrentaz@gmail.com>
2020-04-21 23:46:55 -0300

    Setup postgres connection and db

--- src/main/kotlin/com/example/todo/models/TodoModel.kt
+++ src/main/kotlin/com/example/todo/models/TodoModel.kt
@@ -2,9 +2,12 @@ package com.example.todo.models
 
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-class TodoModel(@Id @GeneratedValue var id: Long? = null,
+class TodoModel(@Id
+                @GeneratedValue(strategy = GenerationType.AUTO)
+                var id: Long? = null,
                 var title: String,
                 var description: String)
